031b590280c97e9454908d511d734dfa44c019a3
Soulyana Lakew <soulyana_lakew@outlook.com>
2024-10-01 17:09:22 -0400

    fixed overaly issue and added api folder

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,12 +1,6 @@
-import { UseChatHelpers } from 'ai/react'
-
-import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/external-link'
-import { IconArrowRight } from '@/components/ui/icons'
-
 export function EmptyScreen() {
   return (
-    <div className="mx-auto max-w-3xl px-4">
+    <div className="mx-auto max-w-3xl px-4 mb-36">
       <div className="flex flex-col items-center gap-4 rounded-lg border bg-background p-8 text-center">
         <h1 className="text-2xl font-bold">
           Welcome to the Product Inventory Chatbot
